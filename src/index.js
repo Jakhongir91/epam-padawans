8a636abe06cc1bee5db2deb6062785fa01ff91e2
Jakhongir Nishanbaev <jakhongir.nishanbaev@gmail.com>
2019-07-03 19:02:18 +0500

    Fixed algorithm, remember triangles instead of deleting ones

--- src/index.js
+++ src/index.js
@@ -4,49 +4,86 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
 
-        let array = decrementIndexes(preferences);
+    let triangles = [];
+    let array = decrementIndexes(preferences);
 
-        let count = 0;
+    let count = 0;
 
-        for (let i=0; i<array.length; i++) {
-            let firstSpichoneey = array[i];
-            let secondSpichoneey = array[firstSpichoneey];
-            let thirdSpichoneey = array[secondSpichoneey];
-            let fourthSpichoneey = array[thirdSpichoneey];
+    for (let i=0; i<array.length; i++) {
+        let firstSpichoneey = array[i];
+        let secondSpichoneey = array[firstSpichoneey];
+        let thirdSpichoneey = array[secondSpichoneey];
+        let fourthSpichoneey = array[thirdSpichoneey];
 
-            if (firstSpichoneey === fourthSpichoneey && (firstSpichoneey !== secondSpichoneey && secondSpichoneey !== thirdSpichoneey)) {
+        if (firstSpichoneey === fourthSpichoneey && (firstSpichoneey !== secondSpichoneey && secondSpichoneey !== thirdSpichoneey)) {
+            let triangle = [];
+            triangle.push(firstSpichoneey);
+            triangle.push(secondSpichoneey);
+            triangle.push(thirdSpichoneey);
+
+            triangle.sort(function(a, b) {
+                return a - b;
+            });
+
+            if (isNewTriangle(triangles, triangle)) {
                 count ++;
 
-                removeValuesFromArray(array, firstSpichoneey);
-                removeValuesFromArray(array, secondSpichoneey);
-                removeValuesFromArray(array, thirdSpichoneey);
-
-                for (let i=0; i<array.length; i++) {
-                    array[i] = array[i] - 3;
-                }
+                triangles.push(triangle);
             }
         }
+    }
 
-        return count;
-
-
-
-
-    // getLoveTrianglesCount([2, 3, 1, 5, 6, 4, 2, 3, 1, 5, 6, 4, 2, 3, 1, 5, 6, 4]);
+    return count;
 };
 
-function removeValuesFromArray(array, value) {
-    let index = array.indexOf(value);
-    while (index !== -1) {
-        array.splice(index, 1);
-        index = array.indexOf(value);
-    }
-}
-
+/**
+ * Привести идексы к удобному для работы с массивами виду
+ *
+ * @param array
+ * @returns {*}
+ */
 function decrementIndexes(array) {
     for (let i=0; i<array.length; i++) {
         array[i] = array[i] - 1;
     }
 
     return array;
+}
+
+/**
+ * Проверяет встречался ли ранее полученный треугольник
+ *
+ * @param triangles
+ * @param triangle
+ * @returns {boolean}
+ */
+function isNewTriangle(triangles, triangle) {
+    for (let i=0; i<triangles.length; i++) {
+        if (triangles[i].length != triangle.length) {
+            continue;
+        }
+
+        if (equalArrays(triangles[i], triangle)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+/**
+ * Проверяет равны ли два массива
+ *
+ * @param array1
+ * @param array2
+ * @returns {boolean}
+ */
+function equalArrays(array1, array2) {
+    for(let j=0; j<array1.length; j++) {
+        if (array1[j] != array2[j]) {
+            return false;
+        }
+    }
+
+    return true;
 }
